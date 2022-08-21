492df7ff785ad9b3c5f2fbd635b8f8de2fa90b89
Mahsum Tarhan <68226220+MahsumTarhan07@users.noreply.github.com>
2022-08-21 21:05:21 +0300

    Cards App

---
+++ cards/script.js
@@ -0,0 +1,35 @@
+
+$(function(){
+    var getTxt = $('.content p').text();
+    var realTxt = [getTxt]
+    var textCol = [getTxt]
+    var wordsCount = textCol[0].split('').lenght;
+    var words = textCol[0].split('');
+    var wordsLimit = 40
+    var limitCol;
+
+
+    $('.readmore h3').click(function(){
+        if( $(this).text() === "show more"){
+            $('.content p').addClass('scroll');
+            $(this).text('show less');
+            $('.item').addClass('showfull');
+        }else{
+            $('.content p').text(limitCol);
+            $(this).text('show more');
+            $('.item').removeClass('showfull');
+        }
+    });
+});
+
+    if(wordsLimit <= wordsCount){
+        words.splice(wordsLimit, wordsCount - wordsLimit);
+        words = words.join('');
+        $('.content p').text(words + '...');
+        limitCol = $('.content p').text();
+    }
+
+    $('.like').click(function(){
+        $(this).toggleClass('clicked');
+        $('.like').css("font-size","17")
+    });
